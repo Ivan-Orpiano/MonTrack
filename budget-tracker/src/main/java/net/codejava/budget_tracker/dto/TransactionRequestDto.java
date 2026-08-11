63d35c711ebc0d3d543ab9d3f2cf137541b8d9c4
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-11 19:41:15 +0800

    P1: Under dev

---
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/dto/TransactionRequestDto.java
@@ -0,0 +1,139 @@
+package net.codejava.budget_tracker.dto;
+
+import net.codejava.budget_tracker.model.TransactionType;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class TransactionRequestDto {
+    
+
+    @NotNull(message = "Data is required!")
+    @PastOrPresent(message = "Data connot be in the future")
+    private LocalDate date;
+
+    @NotNull(message = "Type is required and must be INCOME or EXPENSE")
+    private TransactionType  type;
+
+    @NotBlank(message = "Category is required")
+    @Size(max = 50, message = "Category must be at most 50 characters")
+    private String category;
+
+    @NotBlank(message = "Description is Required!")
+    @Size(max=200, message = "Description must be at most 200 characters")
+    private String description;
+
+    @NotNull(message = "Amount is required!")
+    @DecimalMin(valid= "0.01", message = "Amount must be greater than zero")
+    @Digits(integer = 12, fraction = 2, message = "Amount may have at most 2 decimal places!")
+    private BigDecimal amount;
+    
+
+    public TransactionRequestDto(){
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
