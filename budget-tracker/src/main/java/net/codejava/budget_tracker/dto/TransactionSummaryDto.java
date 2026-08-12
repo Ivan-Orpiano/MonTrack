42646c4b60ecda4bf872565ceeb1649781e69d4b
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-12 12:47:36 +0800

    P1 (Encap): Transaction Summart Details

--- budget-tracker/src/main/java/net/codejava/budget_tracker/dto/TransactionSummaryDto.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/dto/TransactionSummaryDto.java
@@ -20,6 +20,38 @@ public class TransactionSummaryDto {
     }
 
 
+    public BigDecimal getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(BigDecimal totalIncome){
+        this.totalIncome = totalIncome;
+    }
+
+    public BigDecimal getTotalExpense() {
+        return totalExpense;
+    }
+
+    public void setTotalExpense(BigDecimal totalExpense) {
+        this.totalExpense = totalExpense;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+
+    public int getTransactionCount() {
+        return transactionCount;
+    }
+
+    public void setTransactionCount(int transactionCount) {
+        this.transactionCount = transactionCount;
+    }
 
 
 }
