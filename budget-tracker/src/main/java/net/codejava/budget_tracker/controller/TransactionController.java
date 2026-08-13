5de9aa6d193021205f706804dc37110e6d41b1bf
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-13 18:44:04 +0800

    Dev

--- budget-tracker/src/main/java/net/codejava/budget_tracker/controller/TransactionController.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/controller/TransactionController.java
@@ -4,10 +4,14 @@ package net.codejava.budget_tracker.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.validation.Valid;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,6 +20,15 @@ import org.springframework.web.bind.annotation.RestController;
 import net.codejava.budget_tracker.dto.TransactionResponseDto;
 import net.codejava.budget_tracker.model.TransactionType;
 import net.codejava.budget_tracker.service.TransactionService;
+
+
+
+import java.net.URI;
+import java.time.LocalDate;
+import java.util.List;
+
+
+
 
 //REST API Controller 
 @RestController
@@ -50,7 +63,7 @@ public class TransactionController {
     @PostMapping
     public ResponseEntity<TransactionResponseDto> createTransaction(@Valid @RequestBody TransactionRequestDto dto) {
         TransactionReponseDto created = transactionService.createTransaction(dto);
-        return ResponseEntity.created(URI.create("/api/tranactions/" + created.getId())).body(created);
+        return ResponseEntity.created(URI.create("/api/transactions/" + created.getId())).body(created);
     }
 
         
