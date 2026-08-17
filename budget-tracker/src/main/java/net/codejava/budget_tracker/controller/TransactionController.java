7762b18696ef7eaca4a0f544e3ab9ff93bba9c91
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-17 15:17:55 +0800

    Encap: Transaction Core Controller

--- budget-tracker/src/main/java/net/codejava/budget_tracker/controller/TransactionController.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/controller/TransactionController.java
@@ -1,10 +1,9 @@
 package net.codejava.budget_tracker.controller;
 
-
+import java.net.URI;
 import java.time.LocalDate;
 import java.util.List;
 
-import jakarta.validation.Valid;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,19 +16,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import net.codejava.budget_tracker.dto.TransactionRequestDto;
 import net.codejava.budget_tracker.dto.TransactionResponseDto;
 import net.codejava.budget_tracker.dto.TransactionSummaryDto;
 import net.codejava.budget_tracker.model.TransactionType;
 import net.codejava.budget_tracker.service.TransactionService;
-
-
-
-import java.net.URI;
-
-
-
-
 
 //REST API Controller 
 @RestController
@@ -51,14 +43,11 @@ public class TransactionController {
             return ResponseEntity.ok(transactionService.getTransactions(type, category, startDate, endDate));
         }     
 
-
-
     /** GET /api/transactions/{id} */    
     @GetMapping("/{id}")
     public ResponseEntity<TransactionResponseDto> getAllTransactionById(@PathVariable String id) {
         return ResponseEntity.ok(transactionService.getTransactionById(id));
     }
-
 
     /**POST api/transactions */
     @PostMapping
@@ -67,15 +56,12 @@ public class TransactionController {
         return ResponseEntity.created(URI.create("/api/transactions/" + created.getId())).body(created);
     }
 
-
-    /** PUT /api/tansactions/{id} */
+    /** PUT /api/transactions/{id} */
     @PutMapping("/{id}")
     public ResponseEntity<TransactionResponseDto> updateTransaction(
         @PathVariable String id, @Valid @RequestBody TransactionRequestDto dto) {
             return ResponseEntity.ok(transactionService.updateTransaction(id,dto));
         }
-
-
 
     /** DELETE /api/transactions/{id} **/
     @DeleteMapping("/{id}")
@@ -84,7 +70,6 @@ public class TransactionController {
         return ResponseEntity.noContent().build();
     }
     
-
     @GetMapping("/summary")
     public ResponseEntity<TransactionSummaryDto> getSummary(
         @RequestParam(required = false) TransactionType type,
