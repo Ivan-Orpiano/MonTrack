4497f73a002b562d4800d18b53cf03ba8d3ab0ba
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-19 17:09:17 +0800

    Reources Exception

--- budget-tracker/src/main/java/net/codejava/budget_tracker/exception/ResourceNotFoundException.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/exception/ResourceNotFoundException.java
@@ -1,5 +1,9 @@
 package net.codejava.budget_tracker.exception;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends  RuntimeException{
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
     
 }
