7791af71abb13909c2f0e5ab75a1cdd5318521f1
Ivan Ritchel Orpiano <147624545+Ivan-Orpiano@users.noreply.github.com>
2026-08-20 17:47:38 +0800

    Transansaction Encap

--- budget-tracker/src/main/java/net/codejava/budget_tracker/model/Transaction.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/model/Transaction.java
@@ -1,5 +1,86 @@
 package net.codejava.budget_tracker.model;
 
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Transaction {
     
+    private String id;
+    private LocalDate date;
+    private TransactionType type;
+    private String category;
+    private String description;
+    private BigDecimal amount;
+
+
+public Transaction() {
+
+}
+
+public Transaction(String id, LocalDate date, TransactionType type, String category, String description, BigDecimal amount) {
+    this.id = id;
+    this.date = date;
+    this.type = type;
+    this.category = category;
+    this.description = description;
+    this.amount = amount;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+    
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if(this==o) return true;
+        if(!(o instanceof Transaction that)) return false;
+        return Objects.equals(id, this.id);
+    }
+
+
+    @Override
+    public in hashCode(
+
+
+
+
+
+
+
 }
