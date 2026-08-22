4ea22c58aeffb10511f507a6eaaaa2b7d1de5a18
Ivan-Orpiano <ivanritchelorpiano@gmail.com>
2026-08-22 21:39:19 +0800

    P1: Transaction Reposirotry

--- budget-tracker/src/main/java/net/codejava/budget_tracker/repository/TransactionRepository.java
+++ budget-tracker/src/main/java/net/codejava/budget_tracker/repository/TransactionRepository.java
@@ -1,5 +1,19 @@
 package net.codejava.budget_tracker.repository;
 
+import java.util.List;
+
+import net.codejava.budget_tracker.model.Transaction;
+
 public class TransactionRepository {
+
+    public List<Transaction> findAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findAll'");
+    }
+
+    public Transaction update(String id, Transaction transaction) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
     
 }
