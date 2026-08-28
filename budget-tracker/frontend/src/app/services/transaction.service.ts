f3b298829679ec5f325c8ccf6c97e8e48120ceaf
Ivan-Orpiano <ivanritchelorpiano@gmail.com>
2026-08-28 22:45:36 +0800

    P2: Transaction service functions

--- budget-tracker/frontend/src/app/services/transaction.service.ts
+++ budget-tracker/frontend/src/app/services/transaction.service.ts
@@ -1,6 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpParams} from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {filter, Observable} from 'rxjs';
 import {environment} from '../../environments/environment';
 import {Transaction, TransactionRequest} from '../models/transaction.model';
 import {TransactionSummary} from '../models/transaction-summary.model';
@@ -24,6 +24,41 @@ export class TransactionService {
         return this.http.get<Transaction>(`${this.baseUrl}/${id}`);
     }
 
-    
+    createTransaction(transaction: TransactionRequest): Observable<Transaction>{
+        return this.http.post<Transaction>(this.baseUrl, transaction);
+    }
+
+    updateTransaction(id: string, transaction: TransactionRequest): Observable<Transaction>{
+        return this.http.put<Transaction>{`${this.baseUrl}/${id}`, transaction};
+    }
+
+    deleteTransaction(id:string): Observable<void> {
+        return this.http.delete<void>(`${this.baseUrl}/${id}`);
+    }
+
+    getSummary(filter?: TransactionFilter): Observable<TransactionSummary>{
+        return this.http.get<TransactionSummary>(`${this.baseUrl}/summary`, {params: this.buildParamas(filter)});
+    }
+
+    private buildParams(filter?: TranasctionFilter): HttpParams {
+        let params = new HttpParams();
+
+        if(!filter) {
+            return params;
+        }
+        if (filter.type) {
+            params = params.set('type', filter.type);
+        }
+        if (filter.category) {
+            params = params.set('category', filter.category);
+        }
+        if (filter.startDate) {
+            params = params.set('startDate', filter.startDate);
+        }
+        if(filter.endDate) {
+            params = params.set('endDate', filter.endDate);
+        }
+        return params
+    }
 
 }
