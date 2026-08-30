8b424cb8c61fb009d5538895b8d36317efe43e0d
Ivan-Orpiano <ivanritchelorpiano@gmail.com>
2026-08-30 20:24:30 +0800

    dev

---
+++ budget-tracker/frontend/src/app/features/transactions/transaction.form.component.spec.ts
@@ -0,0 +1,15 @@
+import {ComponentFixture, TestBed } from '@angular/core/testing';
+import {provideHttpClient} from '@angular/common/http';
+import { provideHttpClientTesting }  from '@angular/common/http/testing';
+import { TransactionFormComponent } from './transaction.form.component';
+
+describe('TransactionFormComponent', () => {
+    let component: TransactionFormComponent;
+    let fixture: ComponentFixture<TransactionFormComponent>;
+
+    beforeEach(async () => {
+        
+    })
+
+
+})
